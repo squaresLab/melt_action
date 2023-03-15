6621f57733960929ec3ea64fb7222ee32467797c
danieltrt <daniel.r.ramos@icloud.com>
2023-03-15 13:44:42 -0400

    changeS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
 export PYTHONPATH=$PYTHONPATH:/home/test/FixIt
-python3 src/inference/main.py -l --path $1 --base $2 > $1/generated_file.txt
+echo $1
+echo $2
+python3 src/inference/main.py --path $1 --base $2 > $1/generated_file.txt
 echo "rules=generated_file.txt" >> $GITHUB_OUTPUT
