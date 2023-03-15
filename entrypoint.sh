f812f4744ad0612fa7e27b5dd3b981b0abf9dd59
danieltrt <daniel.r.ramos@icloud.com>
2023-03-15 15:47:58 -0400

    v4

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ export PYTHONPATH=$PYTHONPATH:/home/test/FixIt
 echo $1
 echo $2
 echo "Ttying to see head"
+git config --global --add safe.directory /github/workspace
 git show HEAD
 cd /home/test/FixIt/
 python3 src/inference/main.py --path $1 --base $2 > $1/generated_file.txt
